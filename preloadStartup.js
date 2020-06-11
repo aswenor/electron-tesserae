16a6d476643f5cd1a59caed7f79bf0316b1f337a
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-06-11 15:06:15 -0400

    Some comments on loading screen code

--- preloadStartup.js
+++ preloadStartup.js
@@ -1,8 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
   const { ipcRenderer } = require("electron");
+  // on an 'update' event, display the latest message
   ipcRenderer.on('update', (event, msg) => {
     document.getElementById("content").innerHTML += `<p>${msg}</p>`;
   });
+  // on an 'error' event, display the error and stop the loading animation
   ipcRenderer.on('error', (event, msg, err) => {
     document.getElementById("content").innerHTML += `<p>${msg}</p>`;
     document.getElementById("content").innerHTML += `<p>${err}</p>`;
@@ -12,6 +14,7 @@ window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("loading").remove();
   });
 
+  // repeated loading animation
   const animateLoading = () => {
     let curLoad = document.getElementById("loading").innerHTML;
     if (curLoad === null) {
