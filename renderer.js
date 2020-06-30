061851055a77927cda4de2338a1974a3de803d40
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-06-30 15:28:51 -0400

    Try older version of MongoDB for mac

--- renderer.js
+++ renderer.js
@@ -51,7 +51,7 @@ const getMongoDownloadUrl = () => {
     return "https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2012plus-4.2.6.zip";
   }
   if (osname === "darwin") {
-    return "https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-4.2.6.tgz";
+    return "https://fastdl.mongodb.org/osx/mongodb-macosx-ssl-x86_64-4.0.19.tgz";
   }
   if (osname === "linux") {
     // assume Ubuntu 18.04 LTS
