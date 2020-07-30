7ae1e825c928330038b4b25f6f15294618d8e1e4
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-07-30 13:38:58 -0400

    Initialize empty database with base corpus
    Unfortunately, there is a bug with electron that doesn't allow for
    downloading the corpus dump from olivia; will need to re-visit this for
    automatic download of corpus dump.

--- renderer.js
+++ renderer.js
@@ -25,6 +25,9 @@ const PY_DIST_FOLDER = "dist-python"; // python distributable folder
 const PY_SRC_FOLDER = "tisapi"; // path to the python source
 const PY_MODULE = "run_app"; // the name of the main module
 
+// electron thinks olivia's (actually, AddTrust's) certificate is expired
+const CORPUS_DUMP_URL = "https://tess-new.caset.buffalo.edu/standalone/basedump/archivedbasedump20200729.gz";
+
 /**
  * @returns {boolean} is this instance running in a pre-built executable?
  */
@@ -81,8 +84,20 @@ const getMongodPath = () => {
   return mongodPath;
 };
 
+/**
+ * @returns {string} path to mongorestore exectuable
+ */
+const getMongorestorePath = () => {
+  const mongodPath = path.join(getMongodUnpackedPath(), "bin", "mongorestore");
+  if (os.platform() === "win32") {
+    return mongodPath + ".exe";
+  }
+  return mongodPath;
+};
+
 const TESS_HOME = path.join(os.homedir(), "tesserae"); // application home
 const MONGOD_PATH = getMongodPath();
+const MONGORESTORE_PATH = getMongorestorePath();
 const MONGODB_DBPATH = path.join(TESS_HOME, "tessdb");
 
 /**
@@ -360,24 +375,56 @@ const getMongoClient = config => {
 };
 
 /**
- * Ping MongoDB
+ * Ping MongoDB and make sure it is populated
  * @param {Object} config MongoDB configuration
  * @returns {Promise<null>}
  * 
  * If pinging MongoDB fails, application initialization fails.
+ *
+ * If MongoDB is not populated, it will be populated with a base corpus. Any
+ * errors in downloading the base corpus or in populating it will cause
+ * application initialization to fail.
  */
-const checkMongoConnection = (config) => {
-  return new Promise((resolve) => {
-    // Make sure that MongoDB server is reachable
-    const client = getMongoClient(config);
-    client.connect(function(err) {
-      if (err === null) {
-        resolve();
-      } else {
-        writeStartupError("Could not connect to MongoDB", err);
-      }
-    });
-  });
+const checkMongoConnection = async (config) => {
+  const textsCount = await getMongoTextsCount(config)
+  if (textsCount <= 0) {
+    writeStartupMessage("MongoDB is empty");
+    // need to get corpus dump
+    const corpusDumpDest = path.join(TESS_HOME, "archivedbasedump.gz");
+    if (!fs.existsSync(corpusDumpDest)) {
+      writeStartupMessage("Downloading base corpus");
+      await getPromiseViaHttps(CORPUS_DUMP_URL, corpusDumpDest);
+    }
+    try {
+      writeStartupMessage("Attempting to populate MongoDB");
+      // once corpus dump is obtained, load it into MongoDB
+      child_process.execFileSync(
+        MONGORESTORE_PATH,
+        [
+          `--port=${config["port"]}`,
+          '--gzip',
+          `--archive=${corpusDumpDest}`,
+          '--nsFrom="base.*"',
+          '--nsTo="tesserae.*"'
+        ]
+      );
+    } catch (err) {
+      writeStartupError("Could not populate MongoDB with base corpus", err);
+    }
+  }
+};
+
+const getMongoTextsCount = async (config) => {
+  const client = getMongoClient(config);
+  try {
+    var conn = await client.connect();
+    const textsCollection = conn.db('tesserae').collection('texts');
+    return await textsCollection.estimatedDocumentCount({});
+  } catch (err) {
+    writeStartupError("Could not connect to MongoDB", err);
+  } finally {
+    await client.close();
+  }
 };
 
 /**
@@ -441,6 +488,7 @@ const initializeUserSystem = async () => {
 
   writeStartupMessage(`Checking MongoDB connection`);
   await checkMongoConnection(config);
+  writeStartupMessage("MongoDB verification complete");
 
   await installCltkData("lat");
   await installCltkData("grc");
